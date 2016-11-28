89e224496f00829b7e9327db28c0b311ff133ae4
HerczogAttila <herczogattil@gmail.com>
2016-11-28 17:29:21 +0100

    Persist: Begin to use Liquibase and have some refactoring steps

--- src/main/java/com/herczogattila/tlog16rs/TLOG16RSApplication.java
+++ src/main/java/com/herczogattila/tlog16rs/TLOG16RSApplication.java
@@ -5,10 +5,17 @@ import com.herczogattila.tlog16rs.resources.TLOG16RSResource;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import java.io.FileInputStream;
+import java.util.Properties;
 
 public class TLOG16RSApplication extends Application<TLOG16RSConfiguration> {
 
     public static void main(final String[] args) throws Exception {
+        FileInputStream propFile = new FileInputStream("System.properties");
+        Properties p = new Properties(System.getProperties());
+        p.load(propFile);
+        System.setProperties(p);
+        //System.getProperties().list(System.out);
         new TLOG16RSApplication().run(args);
     }
 
--- src/main/java/com/herczogattila/tlog16rs/db/CreateDatabase.java
+++ src/main/java/com/herczogattila/tlog16rs/db/CreateDatabase.java
@@ -9,6 +9,16 @@ import com.avaje.ebean.EbeanServer;
 import com.avaje.ebean.EbeanServerFactory;
 import com.avaje.ebean.config.DataSourceConfig;
 import com.avaje.ebean.config.ServerConfig;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import liquibase.Contexts;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.FileSystemResourceAccessor;
 import org.avaje.agentloader.AgentLoader;
 
 /**
@@ -17,25 +27,36 @@ import org.avaje.agentloader.AgentLoader;
  */
 public class CreateDatabase {
     private final EbeanServer ebeanServer;
+    private Liquibase liquibase;
     
     public CreateDatabase() {
         DataSourceConfig dataSourceConfig = new DataSourceConfig();
-        dataSourceConfig.setDriver("org.mariadb.jdbc.Driver");
-        dataSourceConfig.setUrl("jdbc:mariadb://127.0.0.1:9005/timelogger");
-        dataSourceConfig.setUsername("timelogger");
-        dataSourceConfig.setPassword("633Ym2aZ5b9Wtzh4EJc4pANx");
+        dataSourceConfig.setDriver(System.getProperty("Dsys.prop.driver"));
+        dataSourceConfig.setUrl(System.getProperty("Dsys.prop.url"));
+        dataSourceConfig.setUsername(System.getProperty("Dsys.prop.user"));
+        dataSourceConfig.setPassword(System.getProperty("Dsys.prop.password"));
                 
         ServerConfig serverConfig = new ServerConfig();
-        
-        serverConfig.setName("timelogger");
+        serverConfig.setName(System.getProperty("Dsys.prop.user"));
         serverConfig.setDdlGenerate(true);
         serverConfig.setDdlRun(true);
         serverConfig.setRegister(false);
         serverConfig.setDataSourceConfig(dataSourceConfig);
         serverConfig.addClass(TestEntity.class);
         
+        updateSchema();
         agentLoader();
         ebeanServer = EbeanServerFactory.create(serverConfig);
+    }
+    
+    private void updateSchema() {
+        try {
+            Class.forName(System.getProperty("Dsys.prop.driver"));
+            Connection c = DriverManager.getConnection(System.getProperty("Dsys.prop.url"), System.getProperty("Dsys.prop.user"), System.getProperty("Dsys.prop.password"));
+            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(c));
+            liquibase = new Liquibase("src/main/java/com/herczogattila/tlog16rs/resources/migrations.xml", new FileSystemResourceAccessor(), database);
+            liquibase.update(new Contexts("create"));
+        } catch(ClassNotFoundException | SQLException | LiquibaseException e) { System.out.println("updateSchema: " + e.getMessage()); }
     }
     
     private void agentLoader() {        
