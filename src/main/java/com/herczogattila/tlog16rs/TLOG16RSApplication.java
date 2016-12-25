78f84a63bae7985a84f3c5c4d06a873cfa6cdd47
HerczogAttila <herczogattil@gmail.com>
2016-12-25 23:26:08 +0100

    Sonarqube: Creating a docker image from backend

--- src/main/java/com/herczogattila/tlog16rs/TLOG16RSApplication.java
+++ src/main/java/com/herczogattila/tlog16rs/TLOG16RSApplication.java
@@ -3,16 +3,10 @@ package com.herczogattila.tlog16rs;
 import com.herczogattila.tlog16rs.resources.TLOG16RSResource;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
-import java.io.FileInputStream;
-import java.util.Properties;
 
 public class TLOG16RSApplication extends Application<TLOG16RSConfiguration> {
 
     public static void main(final String[] args) throws Exception {
-        FileInputStream propFile = new FileInputStream("System.properties");
-        Properties p = new Properties(System.getProperties());
-        p.load(propFile);
-        System.setProperties(p);
         new TLOG16RSApplication().run(args);
     }
 
